6631c43e7f83481a038b17f2d078d8624f47afbf
Zachary Coats <zachary4coats@gmail.com>
2023-08-10 16:13:19 -0400

    search bar

--- app/components/SearchItem.js
+++ app/components/SearchItem.js
@@ -1,35 +1,97 @@
 "use client";
-import { BiSearch } from "react-icons/bi";
+import { BiSearch,BiCollapse } from "react-icons/bi";
 import React from "react";
+import { useRouter } from 'next/navigation';
 
 function SearchItem() {
 
-    const [currentSearch,setSearch] = React.useState("r")
-    const unfilteredItems = ["apple earbuds"]
-    const items = unfilteredItems.filter(current =>{
-        return current.includes(currentSearch)
-    })
+
+     let [items,setItems] = React.useState([]);
+     let [active,setActive] = React.useState(false)
+
+     function Activate(e){
+      console.log(e.target)
+      setActive(true)
+     }
+     function Deactivate(){
+      setActive(false)
+     }
+ 
+      const { push } = useRouter()
+      function redirect(card){
+          push(`category/${card.category}/${card.id}`)
+  
+      }
+
+
+    const [currentSearch,setSearch] = React.useState("")
+
+    function query(){
+      push(`search?search=${currentSearch}`)
+
+    }
+
+    async function getData(){
+        items = await fetch(`http://localhost:5000/search/?search=${currentSearch}`,{
+          method: "GET",
+          cache: 'no-cache'
+      
+        })
+        .then(res=> {
+          
+          return res
+        })
+        .then(res=> {    
+            return res.json()
+          })
+
+
+         
+
+          setItems(current=> [...items])
+          
+        
+    }
+    React.useEffect(()=>{
+        if(currentSearch !== ""){
+        getData()
+         }  
+}, [currentSearch])
+  console.log(items)
+    
+    
+    
 
     let currentSearchItems = items.map((item,index)=>{
+      
         return (
-        <div key={Math.random() * 1000} className={`search flex-nowrap flex flex-col  justify-center   h-12  w-12/12 md:w-12/12 
-        ${ (index == items.length -1 && index == 0) && "mt-4 mb-4" ||index == 0 && "mt-4" || index == items.length -1 && "mb-5"   }  hover:bg-slate-500`}>
-                        <h1 className="ml-4">{item}</h1>
-            </div> )  
+        <div key={item.title} className={`search flex-nowrap flex flex-col  justify-center hover:cursor-pointer  h-12  w-12/12 md:w-12/12 
+        ${(items.length == 1 && index == 0) && "mt-4 mb-4" || 
+        index == 0 && "mt-4" || 
+        index == items.length -1 && "mb-5"   }
+          hover:bg-slate-500`} onClick={()=> redirect(item)} >
+            <h1 className="ml-4">{item.Itemtype == "type" ? "Go to ": ""}{item.title}</h1>
+        </div> 
+          )  
 
     })
+    console.log(currentSearchItems)
 
 
     return ( 
        
         <div className='w-10/12 justify-center content-center flex-nowrap flex-col rounded-full '>
-                <div className="search flex-nowrap mx-auto flex flex-row bg-gray-500 h-12 m-4 w-11/12 md:w-8/12 rounded-full">
+                <div onClick={Activate} className="search flex-nowrap mx-auto flex flex-row bg-gray-500 h-12 m-4 w-11/12 md:w-8/12 rounded-full" id="searchbar">
                   <input type="search" onChange={(e)=>setSearch( current=> e.target.value)} value={currentSearch} className='bg-transparent w-10/12 h-full p-3 placeholder:ml focus:outline-none ml-4  border-indigo-500' placeholder='Search for item'></input>
-                  <BiSearch className='w-2/12 h-full m-auto bg-zinc-600 p-3 bg-transparent cursor-pointer rounded-full hover:bg-zinc-900 '/>
+                  <BiSearch onClick={query} className='w-2/12 h-full m-auto bg-zinc-600 p-3 bg-transparent cursor-pointer rounded-full hover:bg-zinc-900 '/>  
                 </div>
+                
                 <div className="flex-nowrap flex mx-auto flex-col relative w-12/12 md:w-8/12 z-50 -mt-2  bg-blue-500 rounded-xl justify-center">
 
-                        { currentSearch !==  "" && currentSearchItems}
+                        { currentSearch !==  ""&& active && currentSearchItems}
+                        
+                            
+
 
                 </div>
         </div> 
---
+++ app/search/page.js
@@ -0,0 +1,59 @@
+
+import { Link } from "react-router-dom"
+import Category from "../components/Category"
+import Item from "../components/item"
+export const metadata = {
+  title: 'Search',
+  description: 'Sorting items by categories',
+}
+
+async function getSearch(query){
+    let items = await fetch(`http://localhost:5000/query/?search=${query}`,{
+      method: "GET",
+      cache: 'no-cache'
+  
+    })
+    .then(res=> {return res})
+    .then(res=> {    return res.json()})
+    return items
+      
+    
+}
+
+
+
+
+
+
+
+export default async function findResults(prop) {
+  console.log(prop)
+    
+    const queryparam1 = prop.searchParams.search;
+
+
+    let data = await getSearch(queryparam1)
+    let items = data.map((current,index) => <Item card={current} border={false} hover={true} key={index}/>)
+   
+    
+    
+    return ( 
+        <div className="flex min-h-screen flex-col">
+            <p className="text-gray-600 text-xs text-center tracking-tighter uppercase"> Results for:</p>
+            <h1 className="hello  text-center text-slate-900 text-4xl mt-2 tracking-tight uppercase pb-4 border-b-4">{queryparam1}</h1>
+            <div className={`flex ${items.length > 0 ?"flex-row": "flex-col" } flex-wrap justify-center`}>
+                {items.length > 0 && items}
+                {items.length == 0 &&
+                 <div className="text-4xl md:text-6xl text-slate-900 m-auto my-36">
+                    <h1>No search Results</h1>
+                </div>
+             
+
+                }
+                
+
+            </div>
+            
+        </div>
+     )
+}
